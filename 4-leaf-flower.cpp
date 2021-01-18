3c85988903d8c9d7781cd18fafdb1f2a0d62ef85
Kartik Sihotiya <sihotiyak.ug19.ec@nitp.ac.in>
2021-01-18 18:53:40 +0530

    Create 4-leaf-flower.cpp

---
+++ 4-leaf-flower.cpp
@@ -0,0 +1,20 @@
+#include<simplecpp>
+
+main_program{
+//Write your code here
+turtleSim();
+repeat(90){
+ forward(2);
+ left(1);}
+ repeat(3){
+ left(90);
+ repeat(180){
+ forward(2);
+ left(1);}
+ }
+ left(90);
+ repeat(90){
+ forward(2);
+ left(1);}
+wait(5);
+}
